c7cf0924d97bc8db251e8828b5f25670c01bdc18
ayush <ayush.tamta8@gmail.com>
2020-07-01 19:26:14 +0530

    added IntervalHook and IntervalClass. they both shows the contrast , and how one can be used in place of other

--- use_effect/src/App.js
+++ use_effect/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HookCounterOne from './components/HookCounterOne'
+import IntervalHookCounter from './components/IntervalHookCounter';
+import IntervalClassCounter from './components/IntervalClassCounter';
 
 
 
 function App() {
   return (
     <div className="App">
-<HookCounterOne />
+<IntervalClassCounter />
+<IntervalHookCounter />
+
     </div>
   );
 }
