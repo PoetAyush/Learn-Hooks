dc5978fd8709fbe5708c6c75fe84a9aa04950365
ayush <ayush.tamta8@gmail.com>
2020-07-01 18:33:27 +0530

    added a click action listner and a text-feild, that could save details and button can count the number of times it has been clicked

--- use_effect/src/components/HookCounterOne.js
+++ use_effect/src/components/HookCounterOne.js
@@ -3,8 +3,16 @@ import React , {useState, useEffect} from 'react'
 function HookCounterOne() {
 
     const [count , setCount] =  useState(0)
+    const [count , setCount] =  useState('')
+
+
+    useEffect(() => {
+        document.title = 'you clicked {count}  times'
+    } , [count]
+     )
     return (
         <div>
+            <input type='text' value={name} onChange={e => setName(e.target.value)} />
             <button onClick={() =>setCount(count +1)}>Click {count} times</button>
         </div>
     )
